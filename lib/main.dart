8e7fc5d9fe1fa9b13d12acff4d1adabebce519d7
Your Name <your.email@example.com>
2025-07-03 15:06:21 +0530

    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:waste_management/UI/Address.dart';
+import 'package:waste_management/UI/Splash_screen.dart';
 import 'package:waste_management/UI/bottomnav.dart';
 import 'package:waste_management/UI/login_page.dart';
 import 'package:waste_management/UI/new_password.dart';
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          // home: SplashScreen(),
-          home: NewPassword(),
+          home: SplashScreen(),
+          // home: NewPassword(),
         );
       },
     );
